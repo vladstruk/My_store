a60cd9eefd15a6c6b825fe32ea7953d760ccdf88
HP <hp@ubuntu.ubuntu-domain>
2015-05-19 23:52:15 +0300

    Adding of error message method

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+  #def error_message obj, field
+  #  if !obj.errors[field].empty?
+  #   "#{field.capitalize} #{obj.errors[field].first}"
+  #  end
+  #end
+
+  def error_message obj, field
+    if message = obj.errors[field].first
+      "#{field.capitalize} #{message}"                  
+    end
+  end
 end
