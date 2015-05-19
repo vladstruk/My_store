ddbc13d209879b6664161195eaaaa68a8942eab4
HP <hp@ubuntu.ubuntu-domain>
2015-05-19 23:32:23 +0300

    Adding Validates method on attribute price

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ require 'pry'
 class Item < ActiveRecord::Base
   attr_accessible :price, :name, :real, :weight, :description
   validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
 
   after_initialize {puts "initialized"}
 end
